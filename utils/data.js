ae7ac1c67827d844d6326d788c0c2d220eaa469c
Maria Silvia <mariasilvia907@gmail.com>
2021-08-24 00:35:42 -0300

    actualizanddata para heroku

--- utils/data.js
+++ utils/data.js
@@ -1,7 +1,7 @@
 const request = require('postman-request');
 
 const getAllExperiences = (callback) => {
-    request('http://localhost:3001/experiences', (error, res, body) => {
+    request('https://experienciasalta-api.herokuapp.com/experiences', (error, res, body) => {
         if(error){
           console.log('Error', error)  
           return callback(error, undefined);
