4d35f7a1f36c1bf2e38b1b24b23886a1c1c24006
Maria Silvia <mariasilvia907@gmail.com>
2021-09-02 20:01:49 -0300

    experiencias

--- routes/experiencias.js
+++ routes/experiencias.js
@@ -1,11 +1,30 @@
 var express = require('express');
 var router = express.Router();
+const data = require('../utils/data')
 
 /* GET experiencias page. */
-router.get('/', function(req, res, next) {
+/* router.get('/', function(req, res, next) {
   res.render('experiencias', {
     title: 'Experiencias'
 });
-});
+}); */
+
+
+router.get('/', function(req, res, next) {
+  data.getAllExperiences((error, data) => {
+    if(error){
+      return res.send({
+        error
+      })
+    }
+    const JSONBody = JSON.parse(data);
+    return res.render('experiencias', { 
+      title: 'Experiencias',
+      slogan: 'Viajá antes de viajar',
+      frase: 'Viví una experiencia 360º',
+      JSONBody  
+    });
+  })
+})
 
 module.exports = router;
