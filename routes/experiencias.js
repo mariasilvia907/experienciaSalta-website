69947836358d04762108b040528463bf8fe00f4a
Maria Silvia <mariasilvia907@gmail.com>
2021-09-02 18:17:04 -0300

    experiencias

--- routes/experiencias.js
+++ routes/experiencias.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET experiencias page. */
 router.get('/', function(req, res, next) {
-  res.render('experiencias', {title: 'Experiencias'});
+  res.render('experiencias', {
+    title: 'Experiencias'
+    slogan: 'Viajá antes de viajar',
+    frase: 'Conocé Salta en 360º'
+});
 });
 
 module.exports = router;
