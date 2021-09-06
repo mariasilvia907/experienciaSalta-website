f5da1dc3e0c927ef79519475522c8ac01631e8b8
Maria Silvia <mariasilvia907@gmail.com>
2021-09-06 10:13:17 -0300

    nosotros image

--- utils/data.js
+++ utils/data.js
@@ -15,6 +15,38 @@ const getAllExperiences = (callback) => {
     });
 }
 
+const getAllBlogNotes = (callback) => {
+    request('https://experienciasalta-api.herokuapp.com/blognotes', (error, res, body) => {
+        if(error){
+          console.log('Error', error)  
+          return callback(error, undefined);
+        }
+        if (res){
+            if(body){
+               return callback(undefined, body);
+             }
+             callback("No se encontraron Notas de Blog", undefined);
+         }
+     });
+}
+
+const getAllMembers = (callback) => {
+    request('https://experienciasalta-api.herokuapp.com/members', (error, res, body) => {
+        if(error){
+          console.log('Error', error)  
+          return callback(error, undefined);
+        }
+        if (res){
+            if(body){
+               return callback(undefined, body);
+            }
+            callback("No se encontraron Miembros del Equipo", undefined);
+        }
+    });
+}
+
 module.exports= {
-   getAllExperiences
+   getAllExperiences,
+   getAllBlogNotes,
+   getAllMembers
 }
