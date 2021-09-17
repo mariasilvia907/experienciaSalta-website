45427b84821b20015a822f0a0ee66324585bcc30
Maria Silvia <mariasilvia907@gmail.com>
2021-09-16 21:35:57 -0300

    actualización index

--- routes/index.js
+++ routes/index.js
@@ -1,38 +1,42 @@
-var express = require('express');
+var express = require("express");
 
 //const { title } = require('node:process');
 
 var router = express.Router();
-const data = require('../utils/data')
+const utils = require("../utils/data");
 
 //const blogNotes = require('../utils/blogNotes')
 
 /* GET home page. */
 
-router.get('/', function(req, res, next) {
-  data.getAllExperiences((error, data) => {
-    if(error){
+router.get("/", function (req, res, next) {
+  utils.getAllExperiences((error, data) => {
+    if (error) {
       return res.send({
-        error
-      })
+        error,
+      });
     }
+
     const JSONBody = JSON.parse(data);
 
-    return res.render('index', { 
-      title: 'Inicio',
-      slogan: 'Viajá antes de viajar',
-      frase: 'Una nueva aventura está por comenzar',
-      JSONBody
-      
+    utils.getAllBlogNotes((error, data) => {
+      if (error) {
+        return res.send({
+          error,
+        });
+      }
+
+      const blogNotes = JSON.parse(data);
+
+      return res.render("index", {
+        title: "Inicio",
+        slogan: "Viajá antes de viajar",
+        frase: "Una nueva aventura está por comenzar",
+        JSONBody,
+        blogNotes,
+      });
     });
   });
-/*   data.getAllblogNotes((error, data) => {
-    if(error){
-      return res.send({
-          error
-        })
-      }
-  }) */
-})
+});
 
 module.exports = router;
